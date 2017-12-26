748e0b99b58d197d5397b60fb451de52d6ac5fb3
ivanevacic <ivanevacicweb@gmail.com>
2017-12-26 05:36:34 +0100

    Added mustache templates

--- Public/js/index.js
+++ Public/js/index.js
@@ -10,20 +10,38 @@ socket.on('disconnect', () => {
 });
 
 socket.on('newMessage', (message)=> {
-  var formattedTime = moment(message.createdAt).format('h:mm a'); //formatted time
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime} : ${message.text}`);
-  jQuery('#messages').append(li);
+  //var formattedTime = moment(message.createdAt).format('h:mm a'); //formatted time
+  //var li = jQuery('<li></li>');
+  //li.text(`${message.from} ${formattedTime} : ${message.text}`);
+  //jQuery('#messages').append(li);
+  
+  //mustache js template
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', (message)=>{
+  //var formattedTime = moment(message.createdAt).format('h:mm a');
+  //var li = jQuery('<li></li>');
+  //var a = jQuery('<a target="_blank">My current location</a>'); //open url in new tab
+  //li.text(`${message.from} ${formattedTime} : `);
+  //a.attr('href', message.url);
+  //li.append(a);
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>'); //open url in new tab
-  li.text(`${message.from} ${formattedTime} : `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html(); //inner html content
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', (e)=> {
